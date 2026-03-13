98d7dfb4364a31a286bf3dae817c4ea170daf29e
Iván Martínez Ruiz <163418612+ivanma777@users.noreply.github.com>
2026-03-13 13:48:28 +0100

    Update Practica1_Programacion2_SantiGonzalez_IvanMartinezRuiz.cpp
    Clases

--- Practica1_Programacion2_SantiGonzalez_IvanMartinezRuiz.cpp
+++ Practica1_Programacion2_SantiGonzalez_IvanMartinezRuiz.cpp
@@ -2,15 +2,48 @@
 //
 
 #include <iostream>
+#ifndef CLASES_H
+#define CLASES_H
+#endif // !CLASES_H
 
 
 #define print std::cout
 
 
-//carlos payasojfkdj
-int main()
+class Animal
 {
-	print << "Hello World!\n";
-	return 0;
-}
+
+
+
+
+};
 //carlos
+
+class Pez : public Animal
+{
+
+
+};
+
+class Oso : public Animal
+{
+
+
+};
+
+
+class rio
+{
+public:
+	/*void rio(Pez* pez, Oso* oso)
+	{
+		print << "El pez se ha alimentado\n";
+	}*/
+
+
+
+};
+
+
+
+//tucu
