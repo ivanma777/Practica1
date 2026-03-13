af797c4f5cdb68268a96b9e61a824b69c73d6865
Iván Martínez Ruiz <163418612+ivanma777@users.noreply.github.com>
2026-03-13 13:08:45 +0100

    prueba

---
+++ Practica1_Programacion2_SantiGonzalez_IvanMartinezRuiz.cpp
@@ -0,0 +1,16 @@
+// Practica1_Programacion2_SantiGonzalez_IvanMartinezRuiz.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+
+#define print std::cout
+
+
+//carlos payasojfkdj
+int main()
+{
+	print << "Hello World!\n";
+	return 0;
+}
+//carlos
